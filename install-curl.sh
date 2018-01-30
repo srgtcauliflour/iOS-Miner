624749c13c20d6182a59b224f6904e83386e9599
Mrtops <Mrtops@users.noreply.github.com>
2018-01-30 17:37:39 -0500

    Delete install-curl.sh

--- install-curl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -ex
-wget https://github.com/curl/curl/releases/download/curl-7_58_0/curl-7.58.0.tar.gz
-tar -xzvf curl-7.58.0.tar.gz
-cd curl-7.58.0 && ./configure --prefix=/usr && make && sudo make install
